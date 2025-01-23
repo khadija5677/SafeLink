ac05081e414979f6dc944a7635cb0e3f35993469
khadija <khadutamboli@gmail.com>
2025-01-23 18:46:48 +0530

    added colour pallete code with color.js file

---
+++ src/Utils/colors.js
@@ -0,0 +1,43 @@
+// colors.js
+export const colors = {
+    // Primary Colors
+    primaryLight: '#5dade2',
+    primary: '#3498db',
+    primaryDark: '#2e86c1',
+  
+    // Secondary Colors
+    secondaryLight: '#58d68d',
+    secondary: '#2ecc71',
+    secondaryDark: '#28b463',
+  
+    // Accent Colors
+    accentLight: '#f1948a',
+    accent: '#e74c3c',
+    accentDark: '#cb4335',
+  
+    // Background Colors
+    backgroundLight: '#f8f9f9',
+    background: '#ecf0f1',
+    backgroundDark: '#d5dbdb',
+  
+    // Text Colors
+    textLight: '#566573',
+    text: '#2c3e50',
+    textDark: '#1c2833',
+  
+    // Border Colors
+    borderLight: '#dcdcdc',
+    border: '#bdc3c7',
+    borderDark: '#95a5a6',
+  
+    // Button Colors
+    buttonPrimary: '#3498db',
+    buttonSecondary: '#2ecc71',
+    buttonAccent: '#e74c3c',
+  
+    // Alert Colors
+    alertSuccess: '#2ecc71',
+    alertWarning: '#f1c40f',
+    alertError: '#e74c3c',
+  };
+  
